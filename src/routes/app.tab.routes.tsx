820c2fad7880b16adfbf64de22ce438bc7707423
GBelther <belthergabriel@gmail.com>
2022-01-24 21:01:58 -0300

    feature: criação das tabs navigations

--- src/routes/app.tab.routes.tsx
+++ src/routes/app.tab.routes.tsx
@@ -1,14 +1,59 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { Profile } from "../screens/Profile/Profile";
+import { useTheme } from "styled-components";
+import { MaterialIcons } from "@expo/vector-icons";
+
 import { AppStackRoutes } from "./app.stack.routes";
+
+import { Profile } from "../screens/Profile";
+import { Cart } from "../screens/Cart";
 
 const { Navigator, Screen } = createBottomTabNavigator();
 
 export function AppTabRoutes() {
+  const theme = useTheme();
+
   return (
-    <Navigator>
-      <Screen name="Home" component={AppStackRoutes} />
-      <Screen name="Profile" component={Profile} />
+    <Navigator
+      initialRouteName="Profile"
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarActiveTintColor: theme.colors.main,
+        tabBarInactiveTintColor: theme.colors.disabled,
+        tabBarStyle: {
+          backgroundColor: theme.colors.background_inverted,
+          borderTopColor: theme.colors.main,
+          borderTopWidth: 2,
+        },
+      }}
+    >
+      <Screen
+        name="Home"
+        component={AppStackRoutes}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons name="local-dining" size={32} color={color} />
+          ),
+        }}
+      />
+      <Screen
+        name="Cart"
+        component={Cart}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons name="shopping-cart" size={32} color={color} />
+          ),
+        }}
+      />
+      <Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons name="account-circle" size={32} color={color} />
+          ),
+        }}
+      />
     </Navigator>
   );
 }
